4bd912b9433f52f82bc292beaafc7b36fbccb53b
Daniel Schulz <danielschulz2005@hotmail.com>
2013-04-07 18:06:18 +0200

    logger level changed

--- WebSphereClearCaches/src/main/java/de/novensa/techniques/maven/plugin/web/as/WebSphere/ClearCachesMojo.java
+++ WebSphereClearCaches/src/main/java/de/novensa/techniques/maven/plugin/web/as/WebSphere/ClearCachesMojo.java
@@ -209,7 +209,7 @@ public class ClearCachesMojo extends MavenLogger implements RuntimeData, ErrorMe
         final File effectivePath = new File(dropEndingString(path, ANY_FILES_WITHIN));
 
         if (!effectivePath.exists()) {
-            log(DEBUG, String.format(DIRECTORY_DOES_NOT_EXIST, effectivePath));
+            log(INFO, String.format(DIRECTORY_DOES_NOT_EXIST, effectivePath));
             cleanedCount++;
             filesToCleanCount++;
             return;
