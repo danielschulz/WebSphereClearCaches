8c105c07e1c891d0a6c85e587e86bd2b5b68f3b1
Daniel Schulz <danielschulz2005@hotmail.com>
2014-04-16 23:24:40 +0200

    cleanly re-initiated

---
+++ WebSphereClearCaches/src/main/java/de/novensa/techniques/maven/plugin/web/as/WebSphere/Runtime/RuntimeData.java
@@ -0,0 +1,18 @@
+package de.novensa.techniques.maven.plugin.web.as.WebSphere.Runtime;
+
+/**
+ * This interface supplies the running plugin with JVM-specific, OS-specific, and other information.
+ *
+ * @author Daniel Schulz
+ */
+@SuppressWarnings("UnusedDeclaration")
+public interface RuntimeData {
+
+    static final String LINE_BREAK = System.getProperty("line.separator");
+    static final String OS_NAME = System.getProperty("os.name");
+    static final String TABULATOR = "\t";
+
+    static final String FILE_SEPARATOR = System.getProperty("file.separator");
+    static final String ANY_FILES_WITHIN = FILE_SEPARATOR + ":*:";              // tells the plugin just to clear the
+    // contained files but not the folder itself
+}
---
+++ WebSphereClearCaches/src/main/java/de/novensa/techniques/maven/plugin/web/as/WebSphere/Utils/LocalRunner/MasterConfiguration.java
@@ -0,0 +1,52 @@
+package de.novensa.techniques.maven.plugin.web.as.WebSphere.Utils.LocalRunner;
+
+import de.novensa.techniques.maven.plugin.web.as.WebSphere.ClearCachesMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * This is the local configuration to run this plugin in local mode and to test run it.
+ *
+ * @author Daniel Schulz
+ */
+public class MasterConfiguration implements PrivateMasterConfigurationsConstants {
+
+
+    /**
+     * the master configuration for your local WebSphere installation
+     */
+    static final ClearCachesMojo mojo = new ClearCachesMojo();
+    static {
+        mojo.setWsHome(WS_HOME);
+        mojo.setWsVersion(WS_VERSION);
+        mojo.setAppServerProfile(WS_APP_SERVER_PROFILE);
+        mojo.setAppServer(WS_APP_SERVER);
+        mojo.setNode(WS_APP_SERVER_NODE);
+        mojo.setCell(WS_APP_SERVER_CELL);
+    }
+
+
+    /**
+     * Returns the master configuration in a <code>ClearCachesMojo</code> instance.
+     *
+     * @return The mojo with the master configuration´s settings
+     */
+    @SuppressWarnings("UnusedDeclaration")
+    public static ClearCachesMojo getMasterConfiguration() {
+        return mojo;
+    }
+
+
+    /**
+     * If you want to run this plugin locally please do so with this configuration.
+     *
+     * @param args Unused command line arguments
+     * @throws MojoExecutionException This exception will be thrown when there is an exception regarding the Runtime
+     *                                of the plugin
+     * @throws MojoFailureException This exception will be thrown when there is an exception regarding the Runtime
+     *                              of the plugin
+     */
+    public static void main(String[] args) throws MojoFailureException, MojoExecutionException {
+        mojo.execute();
+    }
+}
